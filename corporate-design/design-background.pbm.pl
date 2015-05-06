9bc836d217b89f5fb9b020737d37faa5836f4eb2
Stefan Majewsky <majewsky@gmx.net>
2015-05-06 19:48:42 +0200

    add design-background

---
+++ corporate-design/design-background.pbm.pl
@@ -0,0 +1,36 @@
+#!/usr/bin/perl
+use strict;
+use warnings;
+use v5.20;
+
+my $white = '255 255 255 ';
+my $gray  = '240 240 240 ';
+my $fsfw1 = '44 171 183 ';
+my $fsfw2 = '107 186 0 ';
+my $fsfw3 = '90 64 198 ';
+my @colordist = (
+    ($gray) x 20,
+    ($fsfw1) x 4,
+    ($fsfw2) x 1,
+    ($fsfw3) x 1,
+);
+
+my $raster_size = 16;
+my $block_size = 12;
+my $blocks_x = 100;
+my $blocks_y = 100;
+
+my $padding = $raster_size - $block_size;
+my $pixels_x = $blocks_x * $raster_size;
+my $pixels_y = $blocks_y * $raster_size;
+say "P3 $pixels_x $pixels_y 255";
+
+for my $i (1..$blocks_y) {
+    my $blocks_line = '';
+    for my $j (1..$blocks_x) {
+        my $color = $colordist[rand @colordist];
+        $blocks_line .= $color x $block_size . $white x $padding;
+    }
+    say $blocks_line x $block_size;
+    say $white x $padding x $pixels_x;
+}
