0da47267014194bc01acfbcb9da98f172336e64e
cknoll <CarstenKnoll@gmx.de>
2016-09-12 23:51:35 +0200

    Entwurf Gutschein Seite 2 + Korrekturen Seite 1

--- 2016-uni-stick-gutschein/make.sh
+++ 2016-uni-stick-gutschein/make.sh
@@ -4,9 +4,11 @@
 pdflatex margin.tex
 
 pdftk margin.pdf cat 2 output 1.pdf
+pdftk margin.pdf cat 3 output 2.pdf
 
 
 /usr/bin/pdfjam --suffix nup --nup '2x2' -- 1.pdf - 1.pdf - 1.pdf - 1.pdf
+/usr/bin/pdfjam --suffix nup --nup '2x2' -- 2.pdf - 2.pdf - 2.pdf - 2.pdf
 
 
-pdftk 1-nup.pdf cat 1 1 output final.pdf
+pdftk A=1-nup.pdf B=2-nup.pdf cat A B output final.pdf
